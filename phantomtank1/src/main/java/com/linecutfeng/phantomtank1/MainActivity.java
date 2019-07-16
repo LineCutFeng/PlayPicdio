f37087ba41fac685eaa827bfb4226c62f51e990a
932413237@qq.com <1993@feng>
2019-07-17 00:15:20 +0800

    幻影坦克模块

--- phantomtank1/src/main/java/com/linecutfeng/phantomtank1/MainActivity.java
+++ phantomtank1/src/main/java/com/linecutfeng/phantomtank1/MainActivity.java
@@ -2,6 +2,7 @@ package com.linecutfeng.phantomtank1;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -26,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void start() {
-
+        int[] upPix = new int[up.getWidth() * up.getHeight()];
+        int[] downPix = new int[down.getWidth() * down.getHeight()];
+        int[] alpha = upPix.clone();
+        up.getPixels(upPix, 0, up.getWidth(), 0, 0, up.getWidth(), up.getHeight());
+        down.getPixels(downPix, 0, down.getWidth(), 0, 0, down.getWidth(), down.getHeight());
+        for (int i = 0; i < upPix.length; i++) {
+            int color = upPix[i];
+            int r = (color >> 16) & 0xff;
+            int g = (color >> 8) & 0xff;
+            int b = (color) & 0xff;
+            int gray = (int) (r * 0.299 + g * 0.587 + b * 0.114);
+            gray = (int) (128 + (255 - 128) * gray / 255.0f);
+            if (gray > 255)
+                gray = 255;
+            upPix[i] = Color.rgb(gray, gray, gray);
+        }
+        for (int i = 0; i < downPix.length; i++) {
+            int color = downPix[i];
+            int r = (color >> 16) & 0xff;
+            int g = (color >> 8) & 0xff;
+            int b = (color) & 0xff;
+            int gray = (int) (r * 0.299 + g * 0.587 + b * 0.114);
+            gray = (int) (0 + (127 - 0) * gray / 255.0f);
+            downPix[i] = Color.rgb(gray, gray, gray);
+            alpha[i] = 255 + gray - (upPix[i] & 0xff);
+            downPix[i] = Color.argb(alpha[i])
+        }
     }
 }
