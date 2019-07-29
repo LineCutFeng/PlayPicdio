0a42abfeea10052f8e5e61812d768e0febf2291d
932413237 <932413237@qq.com>
2019-07-29 18:20:00 +0800

    ascii码图片视频船新版本，实现彩色ascii图片，视频,效果贼棒

--- watercolor/src/main/java/com/lcf/watercolor/L0SmoothUtils.java
+++ watercolor/src/main/java/com/lcf/watercolor/L0SmoothUtils.java
@@ -1,15 +1,27 @@
 package com.lcf.watercolor;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import java.util.Arrays;
 
 public class L0SmoothUtils {
 
+    int[] rgbi;
+    int[] rgbr;
+    int[] rgbH;
+    int[] rgbV;
+    int[] rgNr;
+    int[] rgbNi;
 
-    public static Bitmap LOSMooth(int width, int height, Bitmap imageData) {
-        int rw = getCeilPower2(width);
-        int rh = getCeilPower2(height);
+    public Bitmap LOSMooth(Bitmap imageData) {
+        int rw = getCeilPower2(imageData.getWidth());
+        int rh = getCeilPower2(imageData.getHeight());
         int maxWidth = 1 << rw;
         int maxHeight = 1 << rh;
+        int[] pixels = new int[imageData.getWidth() * imageData.getHeight()];
+        initRGB(pixels, imageData.getWidth(), imageData.getHeight(), maxWidth, maxHeight);
+        initRGBHV(maxWidth, maxHeight);
         return null;
     }
 
@@ -21,7 +33,26 @@ public class L0SmoothUtils {
         return r;
     }
 
-//    private static void initRGB(imageData, int width, int height, int maxWidth, int maxHeight){
-//
-//    }
+    private void initRGB(int[] pixels, int width, int height, int maxWidth, int maxHeight) {
+        int size = maxWidth * maxHeight;
+        rgbi = new int[size];
+        rgbr = new int[size];
+        Arrays.fill(rgbi, Color.BLACK);
+        Arrays.fill(rgbr, Color.BLACK);
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                int index1 = y * maxWidth + x;
+                int index2 = y * width + x;
+                rgbr[index1] = pixels[index2];
+            }
+        }
+    }
+
+    private void initRGBHV(int maxWidth, int maxHeight) {
+        int size = maxWidth * maxHeight;
+        rgbH = new int[size];
+        rgbV = new int[size];
+        rgNr = new int[size];
+        rgbNi = new int[size];
+    }
 }
